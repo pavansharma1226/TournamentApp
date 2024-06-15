49ee7fdc71a4a4b4f94a7b1c6921612480565c5b
pavansharma1226 <pavansharma1226@gmail.com>
2024-06-15 15:11:08 +0530

    Modified File

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://pavansharma1226:TnBcuX2CBhh8jZn@cluster0.mzsqod3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://pavansharma1226:nWw1nPXroFUrSGoy@movie.e8vz7h5.mongodb.net/?retryWrites=true&w=majority&appName=Movie");
 
 const connection = mongoose.connection;
 
